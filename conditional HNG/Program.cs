cacaaad8c086940393bd442afaa17824e97c4d12
Emmanuella Fatolu <61954156+oceanuella2020@users.noreply.github.com>
2020-03-20 23:25:15 +0100

    Add project files.

---
+++ conditional HNG/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace conditional_HNG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age=0;
+            string brand;
+
+            Console.WriteLine("Welcome to  Ocean's Bar. How old are you?");
+            age = Int32.Parse(Console.ReadLine());
+
+            if(age >= 18)
+            {
+                Console.WriteLine("What brand of Alcohol would you like?");
+                brand = Console.ReadLine();
+                Console.WriteLine("Have a bottle of "+brand);
+            }
+            else if(age < 11)
+            {
+                Console.WriteLine("You're too young to drink. We will contact your Parent/Guardian to come pick you up Kid. ");
+            }
+            else
+            {
+                Console.WriteLine("You're too young to drink. Please take your leave.");
+            }
+        }
+    }
+}
